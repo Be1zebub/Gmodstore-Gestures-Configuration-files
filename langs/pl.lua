abb1be4eb47f8268907a0bd2b623a3b2f861fe66
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-08-15 17:28:39 +0300

    Create pl.lua

---
+++ langs/pl.lua
@@ -0,0 +1,22 @@
+-- author: https://www.gmodstore.com/users/matstef132
+
+return {
+	NpcName = "Sprzedawca gestów", -- npc overhead, also displayed in the menu header
+	Press = "Naciśnij",  -- npc overhead
+
+	-- vendor npc shop menu
+	Purchased = "Zakupiony",
+	Free = "Darmowy",
+	ForThisJobOnly = "Tylko dla tej pracy",
+
+	-- vendor npc tool menu
+	SpawnNPC = "Spawn NPC",
+	ChangeModel = "Zmień Model",
+	ChangeAnimation = "Zmień Animację",
+	Remove = "Usuń",
+
+	-- vendor npc tool popup menues
+	ModelSelector = "Selektor Modeli",
+	AnimationSelector = "Selektor animacji",
+	CopyToClipboard = "Skopiuj do Schowka"
+}
