ff271fead9382db850bb5667fdb472052693e2ae
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-08-15 16:11:25 +0300

    Create de.lua

---
+++ langs/de.lua
@@ -0,0 +1,20 @@
+return {
+    NpcName = "Animations Verkäufer", -- npc overhead, also displayed in the menu header
+    Press = "Drücken",  -- npc overhead
+
+    -- vendor npc shop menu
+    Purchased = "Gekauft",
+    Free = "Kostenlos",
+    ForThisJobOnly = "Nur für diesen Job",
+
+    -- vendor npc tool menu
+    SpawnNPC = "NPC Platzieren",
+    ChangeModel = "Model ändern",
+    ChangeAnimation = "Animation ändern",
+    Remove = "Entfernen",
+
+    -- vendor npc tool popup menues
+    ModelSelector = "Model Wähler",
+    AnimationSelector = "Animation Wähler",
+    CopyToClipboard = "In Zwischenablage kopiert"
+}
