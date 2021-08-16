fd5cfd8b0c14b91cb0ff96e51011f2c13b85044a
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-08-17 01:36:28 +0300

    Create tr.lua

---
+++ langs/tr.lua
@@ -0,0 +1,22 @@
+-- author: https://www.gmodstore.com/users/Wolflix
+
+return {
+	NpcName = "Haraket Satıcısı", -- npc overhead, also displayed in the menu header
+	Press = "Tıkla",  -- npc overhead
+
+	-- vendor npc shop menu
+	Purchased = "Satın Alındı",
+	Free = "Ücretsiz",
+	ForThisJobOnly = "Bu mesleğe özel",
+
+	-- vendor npc tool menu
+	SpawnNPC = "NPC Çıkart",
+	ChangeModel = "Model Değiştir",
+	ChangeAnimation = "Animasyon Değiştir",
+	Remove = "Kaldır",
+
+	-- vendor npc tool popup menues
+	ModelSelector = "Model Seçici",
+	AnimationSelector = "Animasyon Seçici",
+	CopyToClipboard = "Panoya Kopyala"
+}
