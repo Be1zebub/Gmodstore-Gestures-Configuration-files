76d3b3e43c96965321c9450bd7760b88cf265364
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-11-02 16:36:05 +0300

    Add files via upload

---
+++ langs/zh.lua
@@ -0,0 +1,27 @@
+-- author: https://www.gmodstore.com/users/76561197999245696
+
+return {
+	NpcName = "動作商販", -- npc overhead, also displayed in the menu header
+	Press = "按",  -- npc overhead
+
+	-- vendor npc shop menu
+	Purchased = "已購買",
+	Free = "免費",
+	ForThisJobOnly = "這些職業專用",
+
+	-- vendor npc tool menu
+	SpawnNPC = "召出NPC",
+	ChangeModel = "改變模型",
+	ChangeAnimation = "更改動作",
+	Remove = "移除",
+
+	-- vendor npc tool popup menues
+	ModelSelector = "模型選擇器",
+	AnimationSelector = "動作選擇器",
+	CopyToClipboard = "複製到剪貼板",
+
+	NpcNameRefund = "動作退回商販",
+	RefundAlready = "您已經進行了退回操作!",
+	RefundCant = "你的購買列表為空，你沒有東西可以退回。",
+	HowManyRefund = "你已退回 %s"
+}
