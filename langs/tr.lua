f2fa0190ea4b21c3f1193fb98b2c1864a9373540
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-01-23 06:45:07 +0300

    tr.lua fix by onyx#5983
    ty to onyx#5983
    
    `Hi I've updated your turkish translation file for your Gestures addon https://codeshare.io/r9kxl7 I found many errors in the translation.`

--- langs/tr.lua
+++ langs/tr.lua
@@ -1,8 +1,9 @@
 -- author: https://www.gmodstore.com/users/Wolflix
+-- 23.01.2022 editor: https://www.gmodstore.com/users/76561198356384264
 
 return {
-	NpcName = "Haraket Satıcısı", -- npc overhead, also displayed in the menu header
-	Press = "Tıkla",  -- npc overhead
+	NpcName = "Animasyon Satıcısı", -- npc overhead, also displayed in the menu header
+	Press = "TIKLA!",  -- npc overhead
 
 	-- vendor npc shop menu
 	Purchased = "Satın Alındı",
@@ -10,9 +11,9 @@ return {
 	ForThisJobOnly = "Bu mesleğe özel",
 
 	-- vendor npc tool menu
-	SpawnNPC = "NPC Çıkart",
-	ChangeModel = "Model Değiştir",
-	ChangeAnimation = "Animasyon Değiştir",
+	SpawnNPC = "NPC çıkart",
+	ChangeModel = "Model'ini değiştir",
+	ChangeAnimation = "Animasyon'unu değiştir",
 	Remove = "Kaldır",
 
 	-- vendor npc tool popup menues
@@ -20,8 +21,8 @@ return {
 	AnimationSelector = "Animasyon Seçici",
 	CopyToClipboard = "Panoya Kopyala",
 
-	NpcNameRefund = "Gestures refund",
-	RefundAlready = "Zaten geri ödeme yaptınız!",
-	RefundCant = "Satın alma listeniz boş, iade edecek hiçbir şeyiniz yok.",
+	NpcNameRefund = "Animasyon'u iade et!",
+	RefundAlready = "Zaten iade ettiniz!",
+	RefundCant = "Bir şey satın almadığınız için, iade edebileceğiniz bir şey yok.",
 	HowManyRefund = "%s iade ettiniz"
 }
